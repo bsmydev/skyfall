14d6efadb3a19ee7c93f1b0aad74a4689daf27e1
Simon Boisseau <bs.mydev@gmail.com>
2013-10-13 16:54:37 +0200

    Added inertia to fly controls

--- js/flyableobject.js
+++ js/flyableobject.js
@@ -8,6 +8,9 @@ SKY.FlyableObject = function() {
 	this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000);
 	this.add( this.camera );
 
+	this.pitchInertia = 0;
+	this.rollInertia = 0;
+
 	this.yaw = function( degrees )
 	{
 		var matrix = new THREE.Matrix4().makeRotationAxis( this.up, degrees * ( Math.PI / 180 ) );
@@ -39,22 +42,31 @@ SKY.FlyableObject = function() {
 
 	this.updateControls = function()
 	{
+
 		if ( SKY.Controls.UP )
 		{
-			this.pitch( 1 );
+			this.pitchInertia = Math.min( 1, this.pitchInertia + 0.1 );
 		}
 		if ( SKY.Controls.DOWN )
 		{
-			this.pitch( -1 );
+			this.pitchInertia = Math.max( -1, this.pitchInertia - 0.1 );
 		}
 		if ( SKY.Controls.LEFT )
 		{
-			this.roll( 1 );
+			this.rollInertia = Math.min( 2, this.rollInertia + 0.1 );
 		}
 		if ( SKY.Controls.RIGHT )
 		{
-			this.roll( -1 );
+			this.rollInertia = Math.max( -2, this.rollInertia - 0.1 );
 		}
+
+
+		this.pitch( this.pitchInertia );
+		this.roll( this.rollInertia );
+
+		this.pitchInertia = this.pitchInertia * 0.975;
+		this.rollInertia = this.rollInertia * 0.975;
+
 	}
 
 };
